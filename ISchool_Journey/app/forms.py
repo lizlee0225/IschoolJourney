e86b4d6e0d203f7b12d77b85b0cea641fa5e8970
thisisedyip <edward_yip@berkeley.edu>
2016-11-09 23:34:23 -0800

    More front end crap

--- ISchool_Journey/app/forms.py
+++ ISchool_Journey/app/forms.py
@@ -5,6 +5,7 @@ from flask.ext.wtf import Form
 from wtforms import StringField, IntegerField, PasswordField, SelectField
 from flask_wtf.html5 import EmailField
 from wtforms import validators
+import sqlite3 as sql
 
 # existing user login form
 class LoginForm(Form):
@@ -18,9 +19,9 @@ class SignUpForm(Form):
 	fname = StringField('fname', [validators.required()])
 	lname = StringField('lname', [validators.required()])
 
-# class CareerForm(Form):
-#     careers_name = StringField('careers_name', [validators.required()])
-#     with sql.connect("app.db") as con:
-#         con.row_factory = sql.Row
-#         results = con.execute("SELECT * FROM careers").fetchall()
-#     career = SelectField(u'careers_name', choices = [ (str(item[1]), str(item[1])) for item in results])
+class CareerForm(Form):
+    careers_name = StringField('careers_name', [validators.required()])
+    with sql.connect("app.db") as con:
+        con.row_factory = sql.Row
+        results = con.execute("SELECT * FROM careers").fetchall()
+    career = SelectField(u'careers_name', choices = [ (str(item[1]), str(item[1])) for item in results])
